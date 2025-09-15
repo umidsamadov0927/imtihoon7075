7465aa674169436b43d34c959b110b424d4b7d4d
umidsamadov0927 <umidsamadov0927@gmail.com>
2025-09-15 17:13:50 +0500

    umid updated code

---
+++ main.js
@@ -0,0 +1,42 @@
+// 1
+// let oneNumber = prompt("Birimchi son")*1
+// let amal = prompt("+,-,*,/");
+// let twoNumber = prompt("Ikkinchi son")*1
+// if (amal === "+") {
+//     alert(oneNumber + twoNumber);
+// }else if (amal === "-") {
+//     alert(oneNumber - twoNumber);
+// }else if (amal === "*") {
+//     alert(oneNumber * twoNumber);
+// }else if (amal === "/") {
+//     alert(oneNumber / twoNumber);
+// }else {
+//     alert("amal yoki son notug'ri kiritildi")
+// }
+
+
+// 3
+// let number = prompt("son kiriting")*1
+// if (number % 2 === 0) {
+//     alert("Bu juft son");
+// }else if (number % 3 === 0) {
+//     alert("Bu toq son");
+// }
+
+// 4
+// let number = prompt("son kiriting")
+// let b = 0
+// for (let i = 0; i < number.length; i++) {
+//     b += number[i]*1
+// }
+// console.log(b)
+
+// 6
+// let str = prompt('ism kiriting');
+// let str2 = str.split('').reverse().join('');
+// console.log(str2);
+
+let numbers = [1,2313,3,54,2]
+numbers.filter((number) => {
+
+})
